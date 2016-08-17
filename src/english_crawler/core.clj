a9942a14f12c2fec423d3c3396df9c2ef8d5ee4f
shoma2da <marcos.shoichi.matsuda@gmail.com>
2016-08-17 17:46:52 +0900

    RSSからタイトル/URLを読み込む処理を作成

--- src/english_crawler/core.clj
+++ src/english_crawler/core.clj
@@ -1,7 +1,18 @@
 (ns english-crawler.core
+  (:require [feedparser-clj.core :as feedparser])
+  (use clojure.zip)
+  (use clojure.pprint)
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def p pprint)
+
+(def src '(
+  { :name "The Japan Times", :feed-url "http://www.japantimes.co.jp/feed/topstories/" }))
+
+(defn one-src-to-entries [one-src]
+  (def entries (:entries (feedparser/parse-feed (:feed-url one-src))))
+  (def simple-entries(map #(select-keys % [:title :link :site-name]) entries))
+  (map #(assoc % :site-name (:name one-src)) simple-entries))
+
+(defn -main [& args]
+  (p (flatten (map one-src-to-entries src))))
