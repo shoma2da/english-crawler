b35fbc46ecab5866a933584ec0f89d03aca6bf9d
shoma2da <marcos.shoichi.matsuda@gmail.com>
2016-08-19 09:53:42 +0900

    既に保存済みのデータの新規追加は行わない ＆ クロール結果のSlackへのポスト

--- src/english_crawler/core.clj
+++ src/english_crawler/core.clj
@@ -1,3 +1,4 @@
+; Prepare
 (ns english-crawler.core
   (:require [feedparser-clj.core :as feedparser])
   (:require [taika.core :as taika])
@@ -5,34 +6,64 @@
   (:require [clj-slack.chat :as slack])
   (use clojure.pprint)
   (:gen-class))
-
 (def p pprint); for debug
-(def firebase-token (System/getenv "FIREBASE_TOKEN"))
-(def firebase-db-name (System/getenv "FIREBASE_DB_NAME"))
-(def slack-token (System/getenv "SLACK_TOKEN"))
-(def slack-room-name(System/getenv "SLACK_ROOM_NAME"))
 
+
+; Feed data
 (def src '(
   { :name "The Japan Times", :feed-url "http://www.japantimes.co.jp/feed/topstories/" }
   { :name "Japan Today", :feed-url "http://www.japantoday.com/feed" }))
 
-(def connection {:api-url "https://slack.com/api" :token slack-token})
 
-(defn one-src-to-entries [one-src]
-  (def entries (:entries (feedparser/parse-feed (:feed-url one-src))))
-  (def simple-entries(map #(select-keys % [:title :link :site-name]) entries))
-  (map #(assoc % :site-name (:name one-src)) simple-entries))
-
-(def user-auth-token
+; Firebase
+(def firebase-token (System/getenv "FIREBASE_TOKEN"))
+(def firebase-db-name (System/getenv "FIREBASE_DB_NAME"))
+(def firebase-user-auth-token
   (let [token-generator (taika-auth/token-generator firebase-token)
         auth-data {:username "taika" :team_id 100}
         admin? false]
     (taika-auth/create-token token-generator auth-data admin?)))
+(def saved-entries
+  (taika/read firebase-db-name "/news/"))
 
+
+; Slack
+(def slack-token (System/getenv "SLACK_TOKEN"))
+(def slack-room-name(System/getenv "SLACK_ROOM_NAME"))
+(def slack-connection {:api-url "https://slack.com/api" :token slack-token})
+
+
+; Functions
+(defn id [data]
+  (hash (:link data)))
+
+(defn fetch-from-rss [one-src]
+  (def entries (:entries (feedparser/parse-feed (:feed-url one-src))))
+  (def simple-entries(map #(select-keys % [:title :link :site-name]) entries))
+  (def rss-data (map #(assoc % :site-name (:name one-src)) simple-entries))
+  (map (fn [data] { (id data) data }) rss-data))
+
+(defn already-saved [data]
+  (def entry-id (str (first (keys data))))
+  (contains? saved-entries entry-id))
+
+(defn insert-news-to-firebase [data]
+  (taika/update! firebase-db-name "/news" data firebase-user-auth-token))
+
+(defn new-data-message [new-data]
+  (if (= 0 (count new-data))
+    "【クロール結果】新着ニュースはありませんでした"
+    (str "【クロール結果】上記 " (count new-data) " 件の新着ニュースをFirebaseに保存しました")))
+
+(defn post-message-to-slack [message]
+  (slack/post-message slack-connection slack-room-name message))
+
+
+; Main
 (defn -main [& args]
-  (def write-data (flatten (map one-src-to-entries src)))
-  ;TODO ↓すべてupdateしないで、まだ入っていないデータのみにした方が良さそう
-  (doseq [data write-data]
-    (taika/update! firebase-db-name "/news" { (hash (:link data)) data } user-auth-token))
-  ;TODO ↓取得結果の概要などもポストすると良さそう（○○件追加とか）
-  (slack/post-message connection slack-room-name "【英語アプリ】ニュースサイトのクロールが終わりました"))
+  (def rss-data (mapcat fetch-from-rss src))
+  (def new-data (remove already-saved rss-data))
+  (doseq [data new-data]
+    (insert-news-to-firebase data)
+    (post-message-to-slack (:title (first (vals data)))))
+  (post-message-to-slack (new-data-message new-data)))
